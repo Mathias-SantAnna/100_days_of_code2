916e9ab64ffee715872b9f49f9d14b2ef04e8bb8
Mathias Sant'Anna <dark_tricolor@hotmail.com>
2024-10-12 14:47:45 +0100

    restart game logic implemented

--- quiz_mind/main.py
+++ quiz_mind/main.py
@@ -2,7 +2,7 @@ from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
 
-
+#Creates question bank
 question_bank = []
 for question in question_data:
     question_text = question['text']
@@ -10,7 +10,8 @@ for question in question_data:
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
+#Quiz initialize
 quiz = QuizBrain(question_bank)
 
-while quiz.still_has_questions():
-    quiz.next_question()
+# Play the quiz
+quiz.play_quiz()
--- quiz_mind/quiz_brain.py
+++ quiz_mind/quiz_brain.py
@@ -2,6 +2,7 @@ class QuizBrain:
 
     def __init__(self, q_list):
         self.question_number = 0
+        self.score = 0
         self.question_list = q_list
 
     def still_has_questions(self):
@@ -15,7 +16,28 @@ class QuizBrain:
 
     def check_answer(self, user_answer, correct_answer):
         if user_answer.lower() == correct_answer.lower():
+            self.score += 1
             print("Congrats! Correct Answer!")
         else:
             print(f"Your answer is wrong. The correct answer is {correct_answer}.")
+        print(f"Your score is {self.score}.")
 
+    def restart_game(self):
+        while True:
+            if not self.still_has_questions():
+                print(f"\nYour final score is {self.score}.")
+                play_again = input("Do you want to play again? (Y/N): ").lower()
+
+                if play_again == "y":
+                    self.question_number = 0  # Reset question number
+                    self.score = 0  # Reset score
+                    print("Restarting the game...\n")
+                    self.play_quiz()
+                else:
+                    print("Thank you for playing!")
+                    break
+
+    def play_quiz(self):
+        while self.still_has_questions():
+            self.next_question()
+        self.restart_game()
